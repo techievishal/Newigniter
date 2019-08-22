fcdcf610994ed01c34417f64e442e715b18dba46
techievishal <monstervishal@yahoo.com>
2019-08-23 02:44:22 +0530

    updates

--- app/admin/widgets/lists/list_body.php
+++ app/admin/widgets/lists/list_body.php
@@ -1,31 +1,32 @@
 <?php foreach ($records as $record) { ?>
     <tr style="">
         <?php if ($showDragHandle) { ?>
-            <td class="list-action">
+           <!-- <td class="list-action">
                 <div class="btn btn-handle">
                     <i class="fa fa-bars"></i>
                 </div>
-            </td>
+            </td> -->
         <?php } ?>
 
         <?php if ($showCheckboxes) { ?>
             <td class="list-action">
-               <!-- <div class="custom-control custom-checkbox">
-                    <input
+                <div class="custom-control custom-checkbox" style="visibility:hidden" >
+                    <input 
                         type="checkbox"
                         id="<?= 'checkbox-'.$record->getKey() ?>"
                         class="custom-control-input"
                         value="<?= $record->getKey(); ?>" name="checked[]"
                     />
+
                     <label class="custom-control-label" for="<?= 'checkbox-'.$record->getKey() ?>">&nbsp;</label>
-                </div> -->
+                </div> 
             </td>
         <?php } ?>
 
         <?php foreach ($columns as $key => $column) { ?>
             <?php if ($column->type != 'button') continue; ?>
             <td class="list-action <?= $column->cssClass ?>">
-                <?= $this->makePartial('lists/list_button', ['record' => $record, 'column' => $column]) ?>
+                <?= $this->makePartial('lists/list_button', ['record' => $record, 'column' => $columns]) ?>
             </td>
         <?php } ?>
 
@@ -36,6 +37,7 @@
             <td
                 class="list-col-index-<?= $index ?> list-col-name-<?= $column->getName() ?> list-col-type-<?= $column->type ?> <?= $column->cssClass ?>"
             >
+            
                 <?= $this->getColumnValue($record, $column) ?>
             </td>
         <?php } ?>
--- app/admin/widgets/lists/list_button.php
+++ app/admin/widgets/lists/list_button.php
@@ -1,8 +1,52 @@
 <?php
-$buttonAttributes = $this->getButtonAttributes($record, $column);
+
+foreach ($columns as $key => $column) {
+    if($column->columnName == 'staff_status'){
+       if($this->getColumnValue($record, $column)  == 'Enabled'){
+       $btnText = 'Lock';
+       $btnClass = 'btn-success'; 
+       $message = 'Disable'; 
+       }else{
+        $btnText = 'Enter';
+       $btnClass = 'btn-primary';  
+       $message = 'Enable';   
+       }
+       ?>
+        <a class="btn <?= $btnClass ?>" id="<?= $message; ?>_<?= $record->getKey(); ?>" data-request-form="#list-form" data-request="onDelete" data-request-data="_method:'DELETE'" 
+        data-request-confirm="Are you sure you want to <?= $message ?> user" tabindex="0" data-original-title="" title="">
+        <?= $btnText ?>
+        </a>
+        <input
+                        type="text"
+                        id="<?= 'status-'.$record->getKey() ?>"
+                        class="custom-control-input"
+                        value="<?= $this->getColumnValue($record, $column)?>" name="userAvailabilityCheck[<?= $record->getKey() ?>]"
+                    />
+        <?php
+    }
+ }
+ 
 ?>
-<?php if (strlen($buttonAttributes)) { ?>
-    <a class="btn btn-success">
-        Lock
-    </a>
-<?php } ?>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
+<script>
+jQuery("a.btn").click(function(){
+
+    var message = (jQuery(this).attr("id").split('_'));
+    if(message[0] == 'Disable'){
+       //    $("#status-"+message[1]).val('0');
+    }else{
+       // $("#status-"+message[1]).val('1');
+    }
+
+    $(this).prop("checked", 1);
+    $("#checkbox-"+message[1]).prop("checked",1);
+
+
+
+    
+});
+
+</script>
+
+
