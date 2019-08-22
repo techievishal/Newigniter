c613b41c820df8fcd89da2cfb3956343ffb7e4ed
techievishal <monstervishal@yahoo.com>
2019-08-23 02:56:48 +0530

    Updates

--- extensions/igniter/local/components/search/default.php
+++ extensions/igniter/local/components/search/default.php
@@ -1,38 +1,247 @@
-<div id="local-box" class="local-box-fluid" style="margin-top:10%">
+<div id="local-box" class="local-box-fluid">
     <div class="panel panel-local local-search">
         <div class="panel-body">
             <h2 class="text-center"><?= lang('igniter.local::default.text_order_summary'); ?></h2>
-            <span class="search-label sr-only"><?= lang('igniter.local::default.label_search_query'); ?></span>
-            <?php if ($hideSearch) { ?>
-                <a class="btn btn-block btn-primary"
-                   href="<?= restaurant_url($menusPage); ?>"><?= lang('igniter.local::default.text_find'); ?></a>
-            <?php }
-            else { ?>
-                <form
-                    id="location-search"
-                    method="POST"
-                    role="form"
-                    data-request="<?= $searchEventHandler; ?>"
-                >
-                    <div class="input-group postcode-group">
-                        <input
-                            type="text"
-                            id="search-query"
-                            class="form-control text-center postcode-control"
-                            name="search_query"
-                            placeholder="<?= lang('igniter.local::default.label_search_query'); ?>"
-                            value="<?= $location->userPosition()->format(); ?>"
-                        >
-                        <div class="input-group-btn">
-                            <button
-                                type="button"
-                                class="btn btn-primary"
-                                data-control="search-local"
-                            ><?= lang('igniter.local::default.text_find'); ?></button>
-                        </div>
-                    </div>
-                </form>
-            <?php } ?>
+            <!DOCTYPE html >
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <title>Creating a Store Locator on Google Maps</title>
+  <style>
+    /* Always set the map height explicitly to define the size of the div
+     * element that contains the map. */
+    #map {
+      height: 100%;
+    }
+    /* Optional: Makes the sample page fill the window. */
+    html, body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
+ </style>
+  </head>
+  <body style="margin:0px; padding:0px;" onload="initMap()">
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5d5aaab6eb1a6b0be6083af8/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+    <div>
+         <label for="raddressInput">Search location:</label>
+         <input type="text" id="addressInput" size="15"/>
+        <label for="radiusSelect">Radius:</label>
+        <select id="radiusSelect" label="Radius">
+          <option value="50" selected>50 kms</option>
+          <option value="30">30 kms</option>
+          <option value="20">20 kms</option>
+          <option value="10">10 kms</option>
+        </select>
+
+        <input type="button" id="searchButton" value="Search"/>
+    </div>
+    <div><select id="locationSelect" style="width: 10%; visibility: hidden"></select></div>
+    <div id="map" style="width: 100%; height: 90%"></div>
+    <div id="nearbylocations" class="col-md-12"></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script>
+      var map;
+      var markers = [];
+      var infoWindow;
+      var locationSelect;
+      $(document).ready(function(){
+  $("#searchButton").click(function(){
+    var address = document.getElementById("addressInput").value;
+    $.ajax({	
+    url : "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=true&key=AIzaSyAnwljUJMdB8tx2iuGkEpapd2Mn12LBBNs&callback",
+    method: "POST",
+	success:handleData
+	});	
+
+  });
+});
+
+function handleData(data){
+    
+        latitude = data.results[0].geometry.location.lat;
+        longitude= data.results[0].geometry.location.lng;
+        var radius = document.getElementById('radiusSelect').value;
+        var searchUrl = 'storelocator/' + latitude + '/' + longitude + '/' + radius;        
+        
+        $.ajax({
+            url : searchUrl,
+            method: "POST",
+            data:{
+          lat: latitude,
+          lng: longitude,
+          radius: radius
+        },
+            success:showData
+        });	
+}
+
+
+function showData(response){
+
+    var data = JSON.parse(response);
+    debugger;
+    var st = "";
+    $.each(data, function(index){
+       st += "<div class='col-md-3'>";
+       st += "<p>"+data[index].id+"</p>";
+       st += "<p>"+data[index].name+"</p>";
+       st += "<p>"+data[index].address+"</p>";
+       st += "</div>";
+       st +='<hr />';
+    });
+    $("#nearbylocations").html(st);
+    debugger;
+
+
+}
+
+        function initMap() {
+           
+          var sydney = {lat: -33.863276, lng: 151.107977};
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: sydney,
+            zoom: 11,
+            mapTypeId: 'roadmap',
+            mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
+          });
+          infoWindow = new google.maps.InfoWindow();
+
+         // searchButton = document.getElementById("searchButton").onclick = searchLocations;
+
+          locationSelect = document.getElementById("locationSelect");
+          locationSelect.onchange = function() {
+            var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+            if (markerNum != "none"){
+              google.maps.event.trigger(markers[markerNum], 'click');
+            }
+          };
+        }
+
+       function searchLocations() {
+           debugger;
+         var address = document.getElementById("addressInput").value;
+         var geocoder = new google.maps.Geocoder();
+         geocoder.geocode({address: address}, function(results, status) {
+           if (status == google.maps.GeocoderStatus.OK) {
+            searchLocationsNear(results[0].geometry.location);
+           } else {
+             alert(address + ' not found');
+           }
+         });
+       }
+
+       function clearLocations() {
+         infoWindow.close();
+         for (var i = 0; i < markers.length; i++) {
+           markers[i].setMap(null);
+         }
+         markers.length = 0;
+
+         locationSelect.innerHTML = "";
+         var option = document.createElement("option");
+         option.value = "none";
+         option.innerHTML = "See all results:";
+         locationSelect.appendChild(option);
+       }
+
+       function searchLocationsNear(center) {
+         clearLocations();
+
+         var radius = document.getElementById('radiusSelect').value;
+         var searchUrl = 'storelocator.php?lat=' + center.lat() + '&lng=' + center.lng() + '&radius=' + radius;
+         downloadUrl(searchUrl, function(data) {
+           var xml = parseXml(data);
+           var markerNodes = xml.documentElement.getElementsByTagName("marker");
+           var bounds = new google.maps.LatLngBounds();
+           for (var i = 0; i < markerNodes.length; i++) {
+             var id = markerNodes[i].getAttribute("id");
+             var name = markerNodes[i].getAttribute("name");
+             var address = markerNodes[i].getAttribute("address");
+             var distance = parseFloat(markerNodes[i].getAttribute("distance"));
+             var latlng = new google.maps.LatLng(
+                  parseFloat(markerNodes[i].getAttribute("lat")),
+                  parseFloat(markerNodes[i].getAttribute("lng")));
+
+             createOption(name, distance, i);
+             createMarker(latlng, name, address);
+             bounds.extend(latlng);
+           }
+           map.fitBounds(bounds);
+           locationSelect.style.visibility = "visible";
+           locationSelect.onchange = function() {
+             var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+             google.maps.event.trigger(markers[markerNum], 'click');
+           };
+         });
+       }
+
+       function createMarker(latlng, name, address) {
+          var html = "<b>" + name + "</b> <br/>" + address;
+          var marker = new google.maps.Marker({
+            map: map,
+            position: latlng
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            infoWindow.setContent(html);
+            infoWindow.open(map, marker);
+          });
+          markers.push(marker);
+        }
+
+       function createOption(name, distance, num) {
+          var option = document.createElement("option");
+          option.value = num;
+          option.innerHTML = name;
+          locationSelect.appendChild(option);
+       }
+
+       function downloadUrl(url, callback) {
+          var request = window.ActiveXObject ?
+              new ActiveXObject('Microsoft.XMLHTTP') :
+              new XMLHttpRequest;
+
+          request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+              request.onreadystatechange = doNothing;
+              callback(request.responseText, request.status);
+            }
+          };
+
+          request.open('GET', url, true);
+          request.send(null);
+       }
+
+       function parseXml(str) {
+          if (window.ActiveXObject) {
+            var doc = new ActiveXObject('Microsoft.XMLDOM');
+            doc.loadXML(str);
+            return doc;
+          } else if (window.DOMParser) {
+            return (new DOMParser).parseFromString(str, 'text/xml');
+          }
+       }
+
+       function doNothing() {}
+  </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?AIzaSyAnwljUJMdB8tx2iuGkEpapd2Mn12LBBNs&callback=initMap">
+    </script>
+  </body>
+</html>
         </div>
     </div>
 </div>
